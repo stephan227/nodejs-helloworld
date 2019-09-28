d04c3f07ee3060b13694824fea4e5d043d3ae453
Stephane Bochet <s.bochet1@gmail.com>
2019-09-28 17:13:35 +0700

    test5

--- http_server.js
+++ http_server.js
@@ -2,9 +2,7 @@ var http = require('http');
 const { uniqueNamesGenerator } = require('unique-names-generator');
  
 http.createServer(function (req, res) {
-
-  const randomName = uniqueNamesGenerator(); // big_red_donkey
- 
+  const randomName = uniqueNamesGenerator();
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World Test 4 \n' + randomName);
 }).listen(3000, "127.0.0.1");
