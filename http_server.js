d5e51b6265d0c77539db422472de3cf0dc74a096
Stephane Bochet <s.bochet1@gmail.com>
2019-09-28 17:16:07 +0700

    Test 0

--- http_server.js
+++ http_server.js
@@ -4,6 +4,6 @@ const { uniqueNamesGenerator } = require('unique-names-generator');
 http.createServer(function (req, res) {
   const randomName = uniqueNamesGenerator();
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World Test 4 \n' + randomName);
+  res.end('Hello World Test 4 '+ randomName);
 }).listen(3000, "127.0.0.1");
 console.log('Server running at http://127.0.0.1:3000/');
